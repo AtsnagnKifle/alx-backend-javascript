c93fb13ef73f5e2ed9b99c2d5cc041effa392858
ak <ak@localhost.localdomain>
2023-01-02 17:06:06 +0300

    data structure - data manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * creates a set from an array
+ * @param {Array} array - The source array
+ * @author Atsnagn <https://github.com/atsnagnkifle>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
